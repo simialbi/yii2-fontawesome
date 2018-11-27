0f5678556e117e44b392885189df809df6538731
simialbi <simi.albi@outlook.com>
2018-11-27 11:37:04 +0100

    reset icon counter to prevent wrong results with multiple tests

--- tests/unit/MainTest.php
+++ tests/unit/MainTest.php
@@ -7,6 +7,7 @@
 
 namespace yiiunit\extensions\fontawesome;
 
+use rmrevin\yii\fontawesome\component\Icon;
 use rmrevin\yii\fontawesome\FAR;
 use rmrevin\yii\fontawesome\FAS;
 use rmrevin\yii\fontawesome\FontAwesome;
@@ -20,6 +21,7 @@ class MainTest extends TestCase
 
     public function testMain()
     {
+        Icon::$counter = 0;
         $this->assertInstanceOf('rmrevin\yii\fontawesome\FAR', new FAR());
         $this->assertInstanceOf('rmrevin\yii\fontawesome\FontAwesome', new FAR());
 
@@ -34,11 +36,13 @@ class MainTest extends TestCase
 
     public function testIconOutput()
     {
+        Icon::$counter = 0;
         $this->assertEquals((string)FAR::i('cog'), '<svg id="i0" class="svg-inline--fa fa-cog fa-w-16" aria-hidden="true" data-prefix="far" data-icon="cog" data-fa-i2svg="" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M452.515 237l31.843-18.382c9.426-5.441 13.996-16.542 11.177-27.054-11.404-42.531-33.842-80.547-64.058-110.797-7.68-7.688-19.575-9.246-28.985-3.811l-31.785 18.358a196.276 196.276 0 0 0-32.899-19.02V39.541a24.016 24.016 0 0 0-17.842-23.206c-41.761-11.107-86.117-11.121-127.93-.001-10.519 2.798-17.844 12.321-17.844 23.206v36.753a196.276 196.276 0 0 0-32.899 19.02l-31.785-18.358c-9.41-5.435-21.305-3.877-28.985 3.811-30.216 30.25-52.654 68.265-64.058 110.797-2.819 10.512 1.751 21.613 11.177 27.054L59.485 237a197.715 197.715 0 0 0 0 37.999l-31.843 18.382c-9.426 5.441-13.996 16.542-11.177 27.054 11.404 42.531 33.842 80.547 64.058 110.797 7.68 7.688 19.575 9.246 28.985 3.811l31.785-18.358a196.202 196.202 0 0 0 32.899 19.019v36.753a24.016 24.016 0 0 0 17.842 23.206c41.761 11.107 86.117 11.122 127.93.001 10.519-2.798 17.844-12.321 17.844-23.206v-36.753a196.34 196.34 0 0 0 32.899-19.019l31.785 18.358c9.41 5.435 21.305 3.877 28.985-3.811 30.216-30.25 52.654-68.266 64.058-110.797 2.819-10.512-1.751-21.613-11.177-27.054L452.515 275c1.22-12.65 1.22-25.35 0-38zm-52.679 63.019l43.819 25.289a200.138 200.138 0 0 1-33.849 58.528l-43.829-25.309c-31.984 27.397-36.659 30.077-76.168 44.029v50.599a200.917 200.917 0 0 1-67.618 0v-50.599c-39.504-13.95-44.196-16.642-76.168-44.029l-43.829 25.309a200.15 200.15 0 0 1-33.849-58.528l43.819-25.289c-7.63-41.299-7.634-46.719 0-88.038l-43.819-25.289c7.85-21.229 19.31-41.049 33.849-58.529l43.829 25.309c31.984-27.397 36.66-30.078 76.168-44.029V58.845a200.917 200.917 0 0 1 67.618 0v50.599c39.504 13.95 44.196 16.642 76.168 44.029l43.829-25.309a200.143 200.143 0 0 1 33.849 58.529l-43.819 25.289c7.631 41.3 7.634 46.718 0 88.037zM256 160c-52.935 0-96 43.065-96 96s43.065 96 96 96 96-43.065 96-96-43.065-96-96-96zm0 144c-26.468 0-48-21.532-48-48 0-26.467 21.532-48 48-48s48 21.533 48 48c0 26.468-21.532 48-48 48z"></path></svg>');
     }
 
     public function testAdvancedIconMaskOutput()
     {
+        Icon::$counter = 0;
         $this->assertEquals(
             (string)FAS::i('comment')->mask('fas fa-circle')->transform('shrink-8')->fixedWidth(),
             '<svg id="i0" class="fa-fw svg-inline--fa fa-comment fa-w-16" data-fa-mask="fas fa-circle" data-fa-transform="shrink-8" data-prefix="fas" data-icon="comment" data-fa-i2svg="" aria-hidden="true" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><defs><clipPath id="clip-i0"><path d="M256 8C119 8 8 119 8 256s111 248 248 248 248-111 248-248S393 8 256 8z"></path></clipPath><mask id="mask-i0" width="100%" height="100%" x="0" y="0" maskUnits="userSpaceOnUse" maskContentUnits="userSpaceOnUse"><rect width="100%" height="100%" x="0" y="0" fill="white"></rect><g transform="translate(256 256)"><g transform="translate(0, 0) scale(0.5, 0.5) rotate(0 0 0)"><path d="M256 32C114.6 32 0 125.1 0 240c0 49.6 21.4 95 57 130.7C44.5 421.1 2.7 466 2.2 466.5c-2.2 2.3-2.8 5.7-1.5 8.7S4.8 480 8 480c66.3 0 116-31.8 140.6-51.4 32.7 12.3 69 19.4 107.4 19.4 141.4 0 256-93.1 256-208S397.4 32 256 32z" transform="translate(-256 -256)" fill="black"></path></g></g></mask></defs><rect width="100%" height="100%" fill="currentColor" clip-path="url(#clip-i0)" mask="url(#mask-i0)" x="0" y="0"></rect></svg>'
