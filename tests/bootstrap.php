abd490a6a44d8e8197407faf5e62562b87f0bb86
Simon Karlen <simi.albi@outlook.com>
2022-07-28 13:29:08 +0200

    fixed path

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -8,8 +8,8 @@ const YII_DEBUG = true;
 $_SERVER['SCRIPT_NAME'] = '/' . __DIR__;
 $_SERVER['SCRIPT_FILENAME'] = __FILE__;
 
-require_once(__DIR__ . '/../../vendor/yiisoft/yii2/Yii.php');
-require_once(__DIR__ . '/../../vendor/autoload.php');
+require_once(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
+require_once(__DIR__ . '/../vendor/autoload.php');
 
 Yii::setAlias('@yiiunit/extensions/fontawesome', __DIR__);
 Yii::setAlias('@rmrevin/yii/fontawesome', dirname(__DIR__) . '/src');
